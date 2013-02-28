c7c8e8f04734d72eaaacee4638b1497f166dc909
Sonia Hamilton <sonia@snowfrog.net>
2013-02-28 22:18:50 +1100

    separate C code into c_bridge - stringers.go

--- stringers.go
+++ stringers.go
@@ -26,16 +26,7 @@ package gsnmpgo
 
 /*
 #cgo pkg-config: glib-2.0 gsnmp
-#include <gsnmp/ber.h>
-#include <gsnmp/pdu.h>
-#include <gsnmp/dispatch.h>
-#include <gsnmp/message.h>
-#include <gsnmp/security.h>
-#include <gsnmp/session.h>
-#include <gsnmp/transport.h>
-#include <gsnmp/utils.h>
-#include <gsnmp/gsnmp.h>
-#include <stdlib.h>
+#include "c_bridge.h"
 */
 import "C"
 
